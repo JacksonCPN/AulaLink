a2f5a1fe56b0a9c443664da9ab2f16afd9d3aef4
JacksonCPN <jacksoncarlos2015@gmail.com>
2023-10-30 11:00:35 -0300

    Conexão Login com user

--- entrar/entrar.js
+++ entrar/entrar.js
@@ -1,18 +1,17 @@
 const loginForm = document.getElementById("login-form");
-        const errorMessage = document.getElementById("error-message");
+const errorMessage = document.getElementById("error-message");
 
-        loginForm.addEventListener("submit", function (e) {
-            e.preventDefault();
-
-            // Obter valores de entrada do usuário
-            const username = document.getElementById("username").value;
-            const password = document.getElementById("password").value;
-
-            // Realizar validação (exemplo simples)
-            if (username === "seu_nome_de_usuario" && password === "sua_senha") {
-                // Redirecionar o usuário após o login bem-sucedido
-                window.location.href = "/painel_do_usuario.html";
-            } else {
-                errorMessage.textContent = "Nome de usuário ou senha incorretos. Tente novamente.";
-            }
-        });
+loginForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  // Obter valores de entrada do usuário
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  // Realizar validação (exemplo simples)
+  if (username === "teste" && password === "teste") {
+    // Redirecionar o usuário após o login bem-sucedido
+    window.location.href = '/usuario/usuario.html';
+   
+  } else {
+    errorMessage.textContent = "Nome de usuário ou senha incorretos. Tente novamente.";
+  }
+});
