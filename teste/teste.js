f503a935522f3bf0bff5bd5c599c86c66d5e1337
Jackson Nascimento <jacksoncarlos2015@gmail.com>
2023-11-02 10:44:40 -0300

    Ajustes de paginas

--- teste/teste.js
+++
@@ -1,7 +0,0 @@
-// Simulando um banco de dados
-var database = [
-    { id: 1, nome: "João", sobrenome: "Silva" },
-    { id: 2, nome: "Maria", sobrenome: "Santos" },
-    { id: 3, nome: "Pedro", sobrenome: "Ferreira" },
-    // Adicione mais dados conforme necessário
-];
