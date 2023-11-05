b3a9005d5538d1db8f92869e8e039b55e75c2a4b
luizf <luizfelipelf2@gmail.com>
2023-11-05 11:33:37 -0300

    add ajuste

--- conexao/services/AuthService.js
+++ conexao/services/AuthService.js
@@ -1,32 +1,38 @@
 import { Constantes } from "./constantes";
 
-export class AuthService { 
-    login(usuario, senha) {
-        const url = `${Constantes.urlBase}/singin`;
-        const dados = {
-            usuario: usuario,
-            senha: senha
-        };
-    
-        fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(dados),
-            headers: {
-            'Content-Type': 'application/json'
-            }
-        })
-        .then(response => response.json())
-        .then(data => {
+export class AuthService {
+  static login(usuario, senha) {
+    console.log('Chegou aqui')
+
+    const url = `${Constantes.urlBase}/singin`;
+    const dados = {
+      usuario: usuario,
+      senha: senha
+    };
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(dados),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(data => {
         if (data) {
-            console.log('Usuário autenticado:', data);
-    
-            localStorage.setItem('token', data.token);
+          console.log('Usuário autenticado:', data);
+
+          localStorage.setItem('token', data.token);
+           // Redirecionar o usuário após o login bem-sucedido
+          window.location.href = '/usuario/usuario.html';
         } else {
-            console.error('Falha na autenticação:', data.mensagem);
+          console.error('Falha na autenticação:', data.mensagem);
+          errorMessage.textContent = "Nome de usuário ou senha incorretos. Tente novamente.";
         }
-        })
-        .catch(error => {
+      })
+      .catch(error => {
         console.error('Erro na requisição:', error);
-        });
-    }
+        errorMessage.textContent = "Erro na requisição. Tente novamente mais tarde.";
+      });
+  }
 }
